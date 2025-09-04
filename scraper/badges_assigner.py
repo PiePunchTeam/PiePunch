f2327e2708760cac9522c396dd69b299a31c69ff
PiePunchKev <piepunchteam@Gmail.com>
2025-09-04 09:00:05 -0500

    Fine-tune Locksmith, Kickin’ Pot Pie, Can’t Touch This, Pie-thon, Iron Chin, and The Dogwalker to hit ~7.5% distribution

--- scraper/badges_assigner.py
+++ scraper/badges_assigner.py
@@ -73,7 +73,7 @@ def assign_badges():
                 if (td_avg > 2.3) and (career_td_acc > 38) and (ctrl_avg > 180) and (total_fights >= 5):
                     fighter_badges.append('Russian Bear')
                     russian_bear_count += 1
-                if (sub_wins_ratio > 0.12) and (sub_att / total_fights > 0.45) and (total_fights >= 5):
+                if (sub_wins_ratio > 0.13) and (sub_att / total_fights > 0.46) and (total_fights >= 5):
                     fighter_badges.append('Pie-thon')
                     pie_thon_count += 1
                 if (ground_finish_rate > 55) and (ground_landed_per_tko > 15) and (ctrl_avg > 150) and (total_fights >= 5) and (ko_tko_wins > 0):
@@ -85,16 +85,16 @@ def assign_badges():
             if (td_def > 82) and (td_attempts_received_avg < 10):
                 fighter_badges.append('Greasy')
                 greasy_count += 1
-            if (str_def > 58) and (sapm < 3.2) and (total_fights >= 5):
+            if (str_def > 59) and (sapm < 3.1) and (total_fights >= 5):
                 fighter_badges.append('Can’t Touch This')
                 cant_touch_this_count += 1
-            if (kd_received_avg < 0.2) and (ko_loss_rate < 5) and (total_fights >= 5):
+            if (kd_received_avg < 0.15) and (ko_loss_rate < 4) and (total_fights >= 5):
                 fighter_badges.append('Iron Chin')
                 iron_chin_count += 1
-            if (sub_att_received_avg < 0.65) and ((sub_def > 95) or (sub_att_received_avg == 0)) and (never_submitted == 1) and (total_fights >= 5):
+            if (sub_att_received_avg < 0.65) and ((sub_def > 97) or (sub_att_received_avg == 0)) and (never_submitted == 1) and (total_fights >= 5):
                 fighter_badges.append('Locksmith')
                 locksmith_count += 1
-            if (total_fights >= 5) and (total_fight_time_sec / total_fights > 160) and (sig_str_landed_per_sec > 0.23):
+            if (total_fights >= 5) and (total_fight_time_sec / total_fights > 155) and (sig_str_landed_per_sec > 0.22):
                 fighter_badges.append('The Dogwalker')
                 dogwalker_count += 1
             if (five_round_fights >= 1) and (five_round_win_rate > 25) and (five_round_decision_rate > 5) and (five_round_wins >= 1):
